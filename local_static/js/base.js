8655b1c716268c7434c998c0efdf8d7b21b53e63
matthigg <mhh129@gmail.com>
2019-08-31 02:33:41 -0400

    Update navbar UI

--- local_static/js/base.js
+++ local_static/js/base.js
@@ -1,9 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-  // Use the height of the navbar to set the height of the navbar 
-  // spacer
-  const navbar_height = document.querySelector('.navbar').offsetHeight
-  document.querySelector('.navbar-spacer').style.height = navbar_height + 'px'
-  document.querySelector('.landing-page-row').style.height = (screen.height - navbar_height - 60) + 'px'
-
+  //
 })

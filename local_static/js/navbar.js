cdba314c5a04a07a36640f32f2fc2333a801fb5b
matthigg <mhh129@gmail.com>
2019-08-31 19:01:49 -0400

    Update navbar & navbar logo UI to change when user scrolls down

---
+++ local_static/js/navbar.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Minimize logo & darken navbar background when user scrolls down
+  window.addEventListener('scroll', () => {
+    console.log('scroll')
+    if (window.scrollY > 0) {
+      document.querySelector('.navbar').classList.add('solid-background')
+      document.querySelector('.nav-logo-text').classList.add('minimized')
+      document.querySelector('.nav-logo-wand-subtext').classList.add('minimized')
+      document.querySelector('.nav-logo-wand-subtext-wrapper').classList.add('minimized')
+    } else {
+      document.querySelector('.navbar').classList.remove('solid-background')
+      document.querySelector('.nav-logo-text').classList.remove('minimized')
+      document.querySelector('.nav-logo-wand-subtext').classList.remove('minimized')
+      document.querySelector('.nav-logo-wand-subtext-wrapper').classList.remove('minimized')
+    }
+  })
+
+
+})

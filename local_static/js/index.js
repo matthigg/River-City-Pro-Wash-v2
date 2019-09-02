b8d6befab8b681d578a1ed6f41df0629a8fb6e31
matthigg <mhh129@gmail.com>
2019-09-02 09:39:08 -0400

    remove script that hides contact form on index.html

--- local_static/js/index.js
+++ local_static/js/index.js
@@ -7,10 +7,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Hide contact form if screen is too small so that it doesn't
   // bleed into the next section
-  const contact_form = document.querySelector('.contact-form-wrapper')
-  const navbar = document.querySelector('.navbar')
-  const landing_page_footer = document.querySelector('.landing-page-footer')
-  if (contact_form.offsetHeight > window.innerHeight - (navbar.offsetHeight + landing_page_footer.offsetHeight + 60)) {
-    contact_form.style.display = "none"
-  } 
+  // const contact_form = document.querySelector('.contact-form-wrapper')
+  // const navbar = document.querySelector('.navbar')
+  // const landing_page_footer = document.querySelector('.landing-page-footer')
+  // if (contact_form.offsetHeight > window.innerHeight - (navbar.offsetHeight + landing_page_footer.offsetHeight + 60)) {
+  //   contact_form.style.display = "none"
+  // } 
 })

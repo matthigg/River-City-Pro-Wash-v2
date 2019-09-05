ec5bd58153859300f60df146ec2dd1f2266f5a7a
matthigg <mhh129@gmail.com>
2019-09-05 12:34:19 -0400

    Add gtag to 'free quote' button'

--- local_static/js/navbar.js
+++ local_static/js/navbar.js
@@ -15,5 +15,35 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   })
 
+  // User clicked 'Call Now' button -- call phone number
+  document.querySelector('.nav-call-now-button').addEventListener('click', () => {
+    gtag('event', 'click_call_button', {
+      'event_category': 'engagement',
+      'event_label': 'User clicked the call button to make a phone call',
+      'value': '1',
+      'event_callback': function() {
+        console.log('call callback fired')
+        window.open('tel:+1-804-258-0403', '_self')
+      },
+    });
+  })
+
+  // User clicked 'FREE Quote' button -- go to contact page
+  document.querySelector('.nav-free-quote-button').addEventListener('click', (event) => {
+    gtag('event', 'click_free_quote_button', {
+      'event_category': 'engagement',
+      'event_label': 'User clicked the free quote button to go to the Contact Us page',
+      'value': '1',
+      'event_callback': function() {
+        console.log('free quote callback fired')
+        // window.location.href = '/'
+        // console.log(window.location.origin)
+        // console.log(window.location.href.slice(0, -1) + event.target.dataset.url)
+        // console.log(window.location.origin.slice(0, -1) + event.target.dataset.url)
+        // window.open(window.location.href.slice(0, -1) + event.target.dataset.url, '_self')
+        window.open(window.location.origin + event.target.dataset.url, '_self')
+      },
+    });
+  })
 
 })

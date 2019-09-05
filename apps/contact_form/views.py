3fb464d83e900c345e125d8e151f5c1574a8093a
matthigg <mhh129@gmail.com>
2019-09-05 00:43:51 -0400

    Add Amazon SES script

--- apps/contact_form/views.py
+++ apps/contact_form/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render, redirect
 from .forms import CreateContactForm
 from django.contrib import messages
+from .amazon_ses import send_email
 
 def submit(request):
   if request.method == "POST":
+
+    # Send email via Amazon SES
+    send_email(request.POST)
+
+    # Create a contact form using the CreateContactForm model
     form = CreateContactForm(request.POST)
 
     # Save form to database if data is valid, create a messages{} 
     # object with a success message, and redirect to contact.html
     if form.is_valid():
       form.save()
-      # messages.add_message(request, messages.INFO, 'Form is valid!')
+      # messages.add_message(request, messages.INFO, 'Your Message Has Been Sent!')
       return redirect('thanks')
 
     # Django stores error messages in form.error. Create & send a new

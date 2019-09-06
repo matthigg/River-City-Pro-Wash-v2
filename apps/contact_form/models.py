feec1734d2aff4c1d8d5fd99ffbf4b1c94c5e36b
matthigg <mhh129@gmail.com>
2019-09-06 14:00:32 -0400

    Add 'date' to contact form database

--- apps/contact_form/models.py
+++ apps/contact_form/models.py
@@ -3,8 +3,9 @@ from django.db import models
 class ContactForm(models.Model):
   class Meta:
     verbose_name_plural = "Contact Form" # (fix pluralization in admin panel)
-  name = models.CharField(max_length=100)
-  address = models.CharField(max_length=200, default='No data')
-  email = models.CharField(max_length=100)
+  name = models.CharField(max_length=64)
+  address = models.CharField(max_length=200, null=True)
+  email = models.CharField(max_length=64)
   phone = models.CharField(max_length = 24)
-  message = models.TextField()
+  message = models.TextField(max_length = 500)
+  date = models.DateTimeField(auto_now_add=True, null=True)

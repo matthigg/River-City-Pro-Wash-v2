2f4dfd9475485c69f4795b59c7ee810bcf9e0300
matthigg <mhh129@gmail.com>
2019-09-06 22:39:36 -0400

    Update UploadedImages model to include alt tag for each image, as well as timestamp for database

--- apps/uploaded_images/models.py
+++ apps/uploaded_images/models.py
@@ -7,7 +7,9 @@ class UploadedImages(models.Model):
     verbose_name_plural = "Uploaded Images" 
 
   img_category = models.CharField(max_length=64, null=True)
-  img_alt = models.CharField(max_length=64)
-  img_notes = models.CharField(max_length=200, null=True)
+  img_alt_before = models.CharField(max_length=64)
   img_before = models.ImageField(upload_to='images/', null=True)
+  img_alt_after = models.CharField(max_length=64)
   img_after = models.ImageField(upload_to='images/', null=True)
+  img_notes = models.TextField(max_length = 200)
+  date = models.DateTimeField(auto_now_add=True, null=True)

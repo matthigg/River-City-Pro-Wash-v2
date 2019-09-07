3d81cad98a616d6571d97af3ee5a3577e8bc5078
matthigg <mhh129@gmail.com>
2019-09-07 13:11:27 -0400

    Update our-work.js and our-work.html

--- local_static/js/our-work.js
+++ local_static/js/our-work.js
@@ -7,19 +7,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Modals
   //
-  // When a <div> .section-1-baap-group element or any of its children (all of 
-  // which are located in ourwork.html and together represent a 'before & after'
-  // picture group) are clicked, search/scrounge for the appropriate data-id
-  // attribute's value, which identifies that particular 'before & after' picture
-  // group. This data-id value is then used to construct the before & after <img>
-  // src attributes in the re-usable Bootstrap modal HTML markup located in 
-  // ourwork-modals.html. 
-  //
-  // Additionally, this JavaScript will scrounge for the relative static img path 
-  // (ie. /static/img/...) which is stored as data-src in the same HTML element 
-  // as the data-id, and the data-src attribute's value, together with the data-id 
-  // attribute's value is then used to construct a path to the before & after 
-  // images that we're looking for.
+  // Add a 'click' event listener to each 'before-and-after' group of images that
+  // looks for a data-category attribute and, if it finds one, grabs the before &
+  // after image URL's, hosted on AWS S3, and sends them to the modal template for
+  // display.
   const baap_groups = document.querySelectorAll('.our-work-baap-group');
   baap_groups.forEach((baap_group) => {
     baap_group.addEventListener('click', (event) => {
@@ -28,7 +19,6 @@ document.addEventListener('DOMContentLoaded', () => {
         while (element.parentNode) {
           element = element.parentNode;
           if (element.dataset.category) {
-            console.log(element.dataset)
             assignModalBAAPImage(element.dataset.beforeSrc, element.dataset.afterSrc);
             return
           } 
@@ -39,15 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   })
   function assignModalBAAPImage(before_src, after_src) {
-
-    console.log(before_src, after_src)
-
     document.querySelector('.modal-baap-group-img-before').src = before_src
     document.querySelector('.modal-baap-group-img-after').src = after_src
-
-    // const img_src_before = src + '/' + id + '-before.jpg';
-    // const img_src_after = src + '/' + id + '-after.jpg';            
-    // document.querySelector('.modal-baap-group-img-before').src = img_src_before;
-    // document.querySelector('.modal-baap-group-img-after').src = img_src_after;
   }
 })

2e150e06a88aa4739bd7452ad1e76d306a6c9bfd
matthigg <mhh129@gmail.com>
2019-09-08 11:55:07 -0400

    Load landing page background image in index.html from within index.html and not _index.scss

--- config/settings.py
+++ config/settings.py
@@ -192,8 +192,10 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 # subclassing S3Boto3Storage. This creates 2 subdirectories in the S3 bucket and
 # conveniently separates static & media files
 # https://www.caktusgroup.com/blog/2014/11/10/Using-Amazon-S3-to-store-your-Django-sites-static-and-media-files/
-STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+
+if os.environ['RCPW_LOCAL_HOST'] == 'None' or os.environ['RCPW_LOCAL_HOST_IP'] == 'None':
+  STATICFILES_LOCATION = 'static'
+  STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'

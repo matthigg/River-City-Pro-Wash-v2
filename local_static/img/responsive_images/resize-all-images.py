49da0cb624b81cd9de121405dadeee630dadc8b3
matthigg <mhh129@gmail.com>
2019-09-09 13:53:06 -0400

    Update responsive image script, resize-all-images.py

--- local_static/img/responsive_images/resize-all-images.py
+++ local_static/img/responsive_images/resize-all-images.py
@@ -14,7 +14,7 @@ import os, sys
 
 # Ideal image size (in bytes) & number of times to run each resize function
 size_target = 150000
-max_iterations = 2
+max_iterations = 7
 
 # This resize function can be used as a sort of binary search algorithm that 
 # recursively passes values to the quality parameter in order to progressively
@@ -23,27 +23,33 @@ def resize_1x(file, name, dimension, i, quality=50):
 
   # If the size of the file (in bytes) is already below the size_target, return.
   if os.path.getsize(file) < size_target:
-    return print("{} is already less than {}".format(file, size_target))
+    return print("{} is already less than {} bytes".format(file, size_target))
+  if i > max_iterations:
+    return print("Max iterations have been reached for {}".format(file))
 
   # Open the image file, alter its dimensions, and save it to a new image file.
-  im = Image.open(file)
-  im.thumbnail(dimension, Image.ANTIALIAS)
-  new_1x_name = name + '-' + str(dimension[0]) + '.jpg'
-  im.save('x-' + new_1x_name, "JPEG", quality=quality)
+  # Max quality = 95 per documentation. 
+  # https://pillow.readthedocs.io/en/5.1.x/handbook/image-file-formats.html
+  if quality <= 95:
+    im = Image.open(file)
+    im.thumbnail(dimension, Image.ANTIALIAS)
+    new_1x_prefix = '1x-'
+    new_1x_name = new_1x_prefix + name + '-' + str(dimension[0]) + '.jpg'
+    im.save(new_1x_name, "JPEG", quality=quality)
 
-  # Determine whether or not to continue to resize the image.
-  if os.path.getsize('./' + 'x-' + new_1x_name) < size_target and i < max_iterations:
-    print('Resulting image size is LESS than {} bytes:'.format(size_target), os.path.getsize('./' + 'x-' + new_1x_name))
-    resize_1x(file, name, dimension, i + 1)
-  elif os.path.getsize('./' + 'x-' + new_1x_name) > size_target and i < max_iterations:
-    print('Resulting image size is GREATER than {} bytes:'.format(size_target), os.path.getsize('./' + 'x-' + new_1x_name))
-    resize_1x(file, name, dimension, i + 1)
-  else:
-    print('Resulting image size equals {} bytes or max_iterations has been reached:'.format(size_target), os.path.getsize('./' + 'x-' + new_1x_name))
-  return
+    # Determine whether or not to continue to resize the image.
+    if os.path.getsize('./' + new_1x_name) < size_target:
+      print('Resulting image size is LESS than {} bytes:'.format(size_target), os.path.getsize('./' + new_1x_name), 'bytes, quality =', quality)
+      resize_1x(file, name, dimension, i + 1, int(quality + (100 - quality) / 2))
+    elif os.path.getsize('./' + new_1x_name) > size_target:
+      print('Resulting image size is GREATER than {} bytes:'.format(size_target), os.path.getsize('./' + new_1x_name), 'bytes, quality =', quality)
+      resize_1x(file, name, dimension, i + 1, int(quality - (100 - quality) / 2))
+    else:
+      print('Resulting image size equals {} bytes:'.format(size_target), os.path.getsize('./' + new_1x_name), 'bytes, quality =', quality)
+    return
 
 # sizes = [(320, 320), (567, 567), (768, 768), (992, 992), (1200, 1200), (1440, 1440)]
-dimensions = [(320, 320), (567, 567)]
+dimensions = [(320, 320)]
 
 for root, dirs, files in os.walk('.', topdown=False):
   for file in files:

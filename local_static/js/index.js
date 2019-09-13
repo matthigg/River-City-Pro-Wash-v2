e9f2636840b61450a20dd0c4c513ba80152c9424
matthigg <mhh129@gmail.com>
2019-09-13 18:39:04 -0400

    Remove contact form-hiding javascript from index.js

--- local_static/js/index.js
+++ local_static/js/index.js
@@ -2,12 +2,3 @@
 if (window.innerWidth < 768) {
   document.querySelector('.nav-free-quote').style.display = 'block'
 }
-
-// Hide contact form if screen is too small so that it doesn't
-// bleed into the next section
-// const contact_form = document.querySelector('.contact-form-wrapper')
-// const navbar = document.querySelector('.navbar')
-// const landing_page_footer = document.querySelector('.landing-page-footer')
-// if (contact_form.offsetHeight > window.innerHeight - (navbar.offsetHeight + landing_page_footer.offsetHeight + 20)) {
-//   contact_form.style.display = "none"
-// } 

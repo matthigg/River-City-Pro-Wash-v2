1e08e95600475a57ff7a529addd5a56607ed2c09
matthigg <mhh129@gmail.com>
2019-09-13 18:07:40 -0400

    Add 'async' and 'defer' tags to navbar.js and index.js

--- config/settings.py
+++ config/settings.py
@@ -184,6 +184,9 @@ AWS_SECRET_ACCESS_KEY = os.environ['RCPW_AWS_SECRET_ACCESS_KEY']
 # Tell django-storages the domain to use to refer to static files.
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
+# Use to silence some S3Boto3Storage warning
+AWS_DEFAULT_ACL = None
+
 # Tell the staticfiles app to use S3Boto3 storage when writing the collected 
 # static files (when you run `collectstatic`).
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'

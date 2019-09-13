3024119438fc3b629273433a42770b528cd8beb5
matthigg <mhh129@gmail.com>
2019-09-13 18:37:37 -0400

    Add css media rule to hide contact form on index.html if page height is too small, comment out the previous javascript that handled it

--- local_static/js/index.js
+++ local_static/js/index.js
@@ -1,16 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
-  
-  // Show 'FREE Quote' button
-  if (window.innerWidth < 768) {
-    document.querySelector('.nav-free-quote').style.display = 'block'
-  }
+// Show 'FREE Quote' button
+if (window.innerWidth < 768) {
+  document.querySelector('.nav-free-quote').style.display = 'block'
+}
 
-  // Hide contact form if screen is too small so that it doesn't
-  // bleed into the next section
-  const contact_form = document.querySelector('.contact-form-wrapper')
-  const navbar = document.querySelector('.navbar')
-  const landing_page_footer = document.querySelector('.landing-page-footer')
-  if (contact_form.offsetHeight > window.innerHeight - (navbar.offsetHeight + landing_page_footer.offsetHeight + 20)) {
-    contact_form.style.display = "none"
-  } 
-})
+// Hide contact form if screen is too small so that it doesn't
+// bleed into the next section
+// const contact_form = document.querySelector('.contact-form-wrapper')
+// const navbar = document.querySelector('.navbar')
+// const landing_page_footer = document.querySelector('.landing-page-footer')
+// if (contact_form.offsetHeight > window.innerHeight - (navbar.offsetHeight + landing_page_footer.offsetHeight + 20)) {
+//   contact_form.style.display = "none"
+// } 

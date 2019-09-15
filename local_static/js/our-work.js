9424318f5bb9362656858374c6cdfb2e2cc07994
matthigg <mhh129@gmail.com>
2019-09-15 12:23:58 -0400

    Fix bug in our-work.js

--- local_static/js/our-work.js
+++ local_static/js/our-work.js
@@ -5,28 +5,30 @@
 // after image URL's, hosted on AWS S3, and sends them to the modal template for
 // display.
 
-console.log('y')
+document.addEventListener('DOMContentLoaded', () => {
 
-const baap_groups = document.querySelectorAll('.our-work-baap-group');
-baap_groups.forEach((baap_group) => {
-  baap_group.addEventListener('click', (event) => {
-    console.log('click')
-    let element = event.target;
-    if (element.dataset.category === undefined) {
-      while (element.parentNode) {
-        element = element.parentNode;
-        if (element.dataset.category) {
-          assignModalBAAPImage(element.dataset.beforeSrc, element.dataset.afterSrc);
-          return
-        } 
+  const baap_groups = document.querySelectorAll('.our-work-baap-group');
+  console.log(baap_groups)
+  baap_groups.forEach((baap_group) => {
+    baap_group.addEventListener('click', (event) => {
+      let element = event.target;
+      if (element.dataset.category === undefined) {
+        while (element.parentNode) {
+          element = element.parentNode;
+          if (element.dataset.category) {
+            assignModalBAAPImage(element.dataset.beforeSrc, element.dataset.afterSrc);
+            return
+          } 
+        }
+      } else {
+        assignModalBAAPImage(element.dataset.beforeSrc, element.dataset.afterSrc);
       }
-    } else {
-      assignModalBAAPImage(element.dataset.beforeSrc, element.dataset.afterSrc);
-    }
+    })
   })
+  function assignModalBAAPImage(before_src, after_src) {
+    console.log('x')
+    document.querySelector('.modal-baap-group-img-before').src = before_src
+    document.querySelector('.modal-baap-group-img-after').src = after_src
+  }
+
 })
-function assignModalBAAPImage(before_src, after_src) {
-  console.log('x')
-  document.querySelector('.modal-baap-group-img-before').src = before_src
-  document.querySelector('.modal-baap-group-img-after').src = after_src
-}
